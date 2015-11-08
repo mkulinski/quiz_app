17c85fcd8a8d17ea93b4045a5949ea1a8f97e6bd
Michael Kulinski <me@michaelkulinski.com>
2015-11-08 16:36:53 -0600

    styled v1

--- js/app.js
+++ js/app.js
@@ -1,72 +1,72 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	//mini-database
-	var qAndA = {
-		question: [
-			"Which of these foods has the most calories?",
-			"Which one of the following set/rep protocols maximally overloads the greatest number muscle fibers, all other factors being equal?",
-			"There are a number of established principles of training. Three of them are the principles of:",
-			"The primary energy provided to fuel sport and activity participations is provided by:",
-			"The most pure aerobic activity you can engage in is:",
-			"Which one of the following is not a two-jointed muscle?"],
-		choices: [
-			"One medium-size baked potato with a teaspoon of butter", "A four ounce roasted chicken breast (skinless)", "Two ounces of candy corn (approximately 32 pieces)", "A 16oz fountain cola",
-			"Five sets of 5 repetitions at 65% of your 1RM", "Five sets of 10 repetitions at 60% of your 1RM", "Three sets of maximum repetitions (to volitional muscular fatigue) at 75% of your 1RM", "60% of your 1RM x 10 reps, 65% x 8 reps, 70% x 6 reps, and 75% x 4 reps",
-			"Modification, Energy Systems, and Effort", "Overload, Recovery/Adaptation, and Progression", "Volume, Specificity, and Genetics", "Aerobic, Anaerobic, and immediate ATP-PC sources",
-			"Dietary carbohydrates", "Dietary polyunsaturated fats", "Daily vitamins and mineral intake", "Protein supplementation",
-			"A two and a half hour soccer match", "Running a marathon", "Running an ultra-marathon", "Sleeping",
-			"Biceps Femoris", "Vastus Medialis", "Gastrocnemius", "Biceps Brachii"],
-		answers: [4,3,3,2,1,4,2]
-	};
+    //mini-database
+    var qAndA = {
+        question: [
+            "Which of these foods has the most calories?",
+            "Approximately how long would a 180 pound person need to run at a 10-minute per mile pace to burn 350 calories?",
+            "Sarcopenia is the term for: ",
+            "The primary energy provided to fuel sport and activity participations is provided by:",
+            "The most pure aerobic activity you can engage in is:",
+            "Which one of the following is not a two-jointed muscle?"],
+        choices: [
+            "A baked potato with a teaspoon of butter", "A four ounce roasted chicken breast", "Two ounces of candy corn", "A 16oz fountain cola",
+            "18 minutes", "21 minutes", "24 minutes", "35 minutes",
+            "Hypertrophy", "Complete muscle contraction", "Maximal aerobic fatigue", "Age related muscle loss",
+            "Dietary carbohydrates", "Dietary polyunsaturated fats", "Daily vitamins and mineral intake", "Protein supplementation",
+            "A two and a half hour soccer match", "Running a marathon", "Running an ultra-marathon", "Sleeping",
+            "Biceps Femoris", "Vastus Medialis", "Gastrocnemius", "Biceps Brachii"],
+        answers: [4,3,3,1,1,4,2]
+    };
 
-	//global variables to store the number of right and wrong answers
-	var userScore = 0;
+    //global variables to store the number of right and wrong answers
+    var userScore = 0;
 
-	//global variable counters
-	var q = 0;
-	var rb1 = 0;
-	var rb2 = 1;
-	var rb3 = 2;
-	var rb4 = 3;
-	var ans = 0;
-	document.querySelector(".guessButton").addEventListener("click", function(){
+    //global variable counters
+    var q = 0;
+    var rb1 = 0;
+    var rb2 = 1;
+    var rb3 = 2;
+    var rb4 = 3;
+    var ans = 0;
+    document.querySelector(".guessButton").addEventListener("click", function(){
 
-		//Get current setup
-		var questionLive = qAndA.question[q];
-		var rb1Live = qAndA.choices[rb1];
-		var rb2Live = qAndA.choices[rb2];
-		var rb3Live = qAndA.choices[rb3];
-		var rb4Live = qAndA.choices[rb4];
-		var ansLive = qAndA.answers[ans];
+        //Get current setup
+        var questionLive = qAndA.question[q];
+        var rb1Live = qAndA.choices[rb1];
+        var rb2Live = qAndA.choices[rb2];
+        var rb3Live = qAndA.choices[rb3];
+        var rb4Live = qAndA.choices[rb4];
+        var ansLive = qAndA.answers[ans];
 
-		//write live question and answers
-		document.getElementById("questions").innerHTML = "<p>" + questionLive + "</p>";
-		document.getElementById("r1").innerHTML = rb1Live;
-		document.getElementById("r2").innerHTML = rb2Live;
-		document.getElementById("r3").innerHTML = rb3Live;
-		document.getElementById("r4").innerHTML = rb4Live;
+        //write live question and answers
+        document.getElementById("questions").innerHTML = "<p>" + questionLive + "</p>";
+        document.getElementById("r1").innerHTML = rb1Live;
+        document.getElementById("r2").innerHTML = rb2Live;
+        document.getElementById("r3").innerHTML = rb3Live;
+        document.getElementById("r4").innerHTML = rb4Live;
 
-		//progress the question and answer choice counters
-		q = q + 1;
-		rb1 = rb1 + 4;
-		rb2 = rb2 + 4;
-		rb3 = rb3 + 4;
-		rb4 = rb4 + 4;
-		ans = ans + 1;
+        //progress the question and answer choice counters
+        q = q + 1;
+        rb1 = rb1 + 4;
+        rb2 = rb2 + 4;
+        rb3 = rb3 + 4;
+        rb4 = rb4 + 4;
+        ans = ans + 1;
 
-		//checks if answer is right or wrong, gets score and alerts user
-		var radio = document.querySelector('#options input[type=radio]:checked');
-		if (radio.value == ansLive) {
-			userScore++;
-			alert("You are correct!");
-			document.getElementById("score").innerHTML = "Score: " + userScore;
-		} else {
-			alert("Wrong answer!");
-		}
+        //checks if answer is right or wrong, gets score and alerts user
+        var radio = document.querySelector('#options input[type=radio]:checked');
+        if (radio.value == ansLive) {
+            userScore++;
+            alert("You are correct!");
+            document.getElementById("score").innerHTML = "Correct Answers: " + userScore;
+        } else {
+            alert("Wrong answer!");
+        }
 
-		//clear radio button
-		if (radio.checked == true) {
-			radio.checked = false;
-		}
-	});
+        //clear radio button
+        if (radio.checked == true) {
+            radio.checked = false;
+        }
+    });
 });
--- js/app2.js
+++ js/app2.js
@@ -1,71 +1,101 @@
+document.addEventListener("DOMContentLoaded", function () {
 
-$(document).ready(function(){
-  var count = 1;
-  var randomNumber = Math.floor((Math.random() * 100) + 1);
+	//mini-database
+	var questionsAndAnswers = {
+		question: [
+			"Which of these foods has the most calories?",
+			"Which one of the following set/rep protocols maximally overloads the greatest number muscle fibers, all other factors being equal?",
+			"There are a number of established principles of training. Three of them are the principles of:",
+			"The primary energy provided to fuel sport and activity participations is provided by:",
+			"The most pure aerobic activity you can engage in is:",
+			"Which one of the following is not a two-jointed muscle?"],
+		choices: [
+			"One medium-size baked potato with a teaspoon of butter", "A four ounce roasted chicken breast (skinless)", "Two ounces of candy corn (approximately 32 pieces)", "A 16oz fountain cola",
+			"Five sets of 5 repetitions at 65% of your 1RM", "Five sets of 10 repetitions at 60% of your 1RM", "Three sets of maximum repetitions (to volitional muscular fatigue) at 75% of your 1RM", "60% of your 1RM x 10 reps, 65% x 8 reps, 70% x 6 reps, and 75% x 4 reps",
+			"Modification, Energy Systems, and Effort", "Overload, Recovery/Adaptation, and Progression", "Volume, Specificity, and Genetics", "Aerobic, Anaerobic, and immediate ATP-PC sources",
+			"Dietary carbohydrates", "Dietary polyunsaturated fats", "Daily vitamins and mineral intake", "Protein supplementation",
+			"A two and a half hour soccer match", "Running a marathon", "Running an ultra-marathon", "Sleeping",
+			"Biceps Femoris", "Vastus Medialis", "Gastrocnemius", "Biceps Brachii"],
+		answers: [4, 3, 3, 2, 1, 4, 2]
+	};
 
-//New Game
-  $(".new").click(function(){
-    $("#userGuess").val('');
-    $("#count").text('0');
-    $("#feedback").text("Make your Guess!");
-    randomNumber = Math.floor((Math.random() * 100) + 1);
-  });
-    //require user input a number between 1 and 100
-	$("input").on("change", function() {
-		var initGuess = $("#userGuess").val();
-		if (initGuess > 100 || initGuess < 1) {
-		alert("Enter a Number Between 1 and 100");
+	/*var questionsAndAnswers = [
+	 {
+	 question: "Which of these foods has the most calories?",
+	 answers: [
+	 "One medium-size baked potato with a teaspoon of butter",
+	 "A four ounce roasted chicken breast (skinless)",
+	 "Two ounces of candy corn (approximately 32 pieces)",
+	 "A 16oz fountain cola"
+	 ],
+	 theGoodOne: 4
+	 },
+	 {
+	 question: "Which of these foods has the most calories?",
+	 answers: [
+	 "One medium-size baked potato with a teaspoon of butter",
+	 "A four ounce roasted chicken breast (skinless)",
+	 "Two ounces of candy corn (approximately 32 pieces)",
+	 "A 16oz fountain cola"
+	 ],
+	 theGoodOne: 4
+	 },
+
+	 ];*/
+
+	//global variables to store the number of right and wrong answers
+	var userScore = 0;
+
+	//global variable counters
+	var q = 0;
+	var rb1 = 0;
+	var rb2 = 1;
+	var rb3 = 2;
+	var rb4 = 3;
+	var ans = 0;
+	document.querySelector(".guessButton").addEventListener("click", function () {
+		/*var questionLive =  questionsAndAnswers[q].question;
+		 var id;
+		 for(var i =  0; i <questionsAndAnswers.answers.length; ++i) {
+		 id =  "r" + (i+1)
+		 document.getElementById[id].innerHTML = questionsAndAnswers[q].answers[i];
+		 }
+
+		 questionsAndAnswers[q].theGoodOne;
+
+		 */
+
+		//write live question and answers
+		document.getElementById("questions").innerHTML = "<p>" + questionsAndAnswers.question[q]; + "</p>";
+		document.getElementById("r1").innerHTML = questionsAndAnswers.choices[rb1];
+		document.getElementById("r2").innerHTML = questionsAndAnswers.choices[rb2];
+		document.getElementById("r3").innerHTML = questionsAndAnswers.choices[rb3];
+		document.getElementById("r4").innerHTML = questionsAndAnswers.choices[rb4];
+
+		//progress the question and answer choice counters
+		q = q + 1;
+		rb1 = rb1 + 4;
+		rb2 = rb2 + 4;
+		rb3 = rb3 + 4;
+		rb4 = rb4 + 4;
+		ans = ans + 1;
+
+		//checks if answer is right or wrong, gets score and alerts user
+		var radio = document.querySelector('#options input[type=radio]:checked');
+		if (radio.value == questionsAndAnswers.answers[ans]) {
+			userScore++;
+			alert("You are correct!");
+			document.getElementById("score").innerHTML = "Score: " + userScore;
+		} else {
+			alert("Wrong answer!");
 		}
+
+		//clear radio button
+		if (radio.checked == true) {
+			radio.checked = false;
+		}
+
+		//radio.checked = false;
+		//radio.checked = !radio.checked;
 	});
- //grab input from user and display how close they are
-  $("#guessButton").click(function(e){
-  	//init variables
-  	var initGuess = $("#userGuess").val();
-	var guess = Math.abs(initGuess - randomNumber);
-
-  	//add each guess to the guesslist
-    $(function addToGuestList() {
-    	$("#guessList").append('<li>'+ initGuess + '</li>');
-    });
-
-    //tells them how close they are
-    $(function userInput() {
-		  if (guess == 0) {
-		    $("#feedback").text("You Got It!");
-		  } else if (guess <= 10){
-		    $("#feedback").text("close!");
-		  } else if (guess <= 25) {
-		  	$("#feedback").text("almost there");
-		  } else if (guess <= 50) {
-		  	$("#feedback").text("still kinda far");
-		  } else if (guess <= 100) {
-		  	$("#feedback").text("so far");
-		  } else {
-		  	$("#feedback").text("So Close It Hurts");
-		  }
-    });
-    
-    //counter
-    $(function counter(){
-      var userNumber = $("#userGuess").val();
-      $("#count").text(count);
-      count++
-    });
-  
-    e.preventDefault();
-  });
-
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
-
-  	});
-
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
-
 });
-
-
