bee655b06d4257e0697dbf60d649713b4a5041fc
Michael Kulinski <me@michaelkulinski.com>
2015-11-18 13:44:11 -0600

    Try this out

--- js/app2.js
+++ js/app2.js
@@ -1,25 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 
 	//mini-database
-	var questionsAndAnswers = {
-		question: [
-			"Which of these foods has the most calories?",
-			"Which one of the following set/rep protocols maximally overloads the greatest number muscle fibers, all other factors being equal?",
-			"There are a number of established principles of training. Three of them are the principles of:",
-			"The primary energy provided to fuel sport and activity participations is provided by:",
-			"The most pure aerobic activity you can engage in is:",
-			"Which one of the following is not a two-jointed muscle?"],
-		choices: [
-			"One medium-size baked potato with a teaspoon of butter", "A four ounce roasted chicken breast (skinless)", "Two ounces of candy corn (approximately 32 pieces)", "A 16oz fountain cola",
-			"Five sets of 5 repetitions at 65% of your 1RM", "Five sets of 10 repetitions at 60% of your 1RM", "Three sets of maximum repetitions (to volitional muscular fatigue) at 75% of your 1RM", "60% of your 1RM x 10 reps, 65% x 8 reps, 70% x 6 reps, and 75% x 4 reps",
-			"Modification, Energy Systems, and Effort", "Overload, Recovery/Adaptation, and Progression", "Volume, Specificity, and Genetics", "Aerobic, Anaerobic, and immediate ATP-PC sources",
-			"Dietary carbohydrates", "Dietary polyunsaturated fats", "Daily vitamins and mineral intake", "Protein supplementation",
-			"A two and a half hour soccer match", "Running a marathon", "Running an ultra-marathon", "Sleeping",
-			"Biceps Femoris", "Vastus Medialis", "Gastrocnemius", "Biceps Brachii"],
-		answers: [4, 3, 3, 2, 1, 4, 2]
-	};
-
-	/*var questionsAndAnswers = [
+	var questionsAndAnswers = [
 	 {
 	 question: "Which of these foods has the most calories?",
 	 answers: [
@@ -39,9 +21,9 @@ document.addEventListener("DOMContentLoaded", function () {
 	 "A 16oz fountain cola"
 	 ],
 	 theGoodOne: 4
-	 },
+	 }
 
-	 ];*/
+	 ];
 
 	//global variables to store the number of right and wrong answers
 	var userScore = 0;
@@ -54,23 +36,19 @@ document.addEventListener("DOMContentLoaded", function () {
 	var rb4 = 3;
 	var ans = 0;
 	document.querySelector(".guessButton").addEventListener("click", function () {
-		/*var questionLive =  questionsAndAnswers[q].question;
-		 var id;
-		 for(var i =  0; i <questionsAndAnswers.answers.length; ++i) {
-		 id =  "r" + (i+1)
-		 document.getElementById[id].innerHTML = questionsAndAnswers[q].answers[i];
-		 }
 
-		 questionsAndAnswers[q].theGoodOne;
 
-		 */
 
 		//write live question and answers
-		document.getElementById("questions").innerHTML = "<p>" + questionsAndAnswers.question[q]; + "</p>";
-		document.getElementById("r1").innerHTML = questionsAndAnswers.choices[rb1];
-		document.getElementById("r2").innerHTML = questionsAndAnswers.choices[rb2];
-		document.getElementById("r3").innerHTML = questionsAndAnswers.choices[rb3];
-		document.getElementById("r4").innerHTML = questionsAndAnswers.choices[rb4];
+		var questionLive =  questionsAndAnswers[q].question;
+		var id;
+		for(var i =  0; i <questionsAndAnswers.answers.length; ++i) {
+			id =  "r" + (i+1);
+			document.getElementById[id].innerHTML = questionsAndAnswers[q].answers[i];
+		}
+
+		questionsAndAnswers[q].theGoodOne;
+
 
 		//progress the question and answer choice counters
 		q = q + 1;
